c553a3d07c14185a2a4eaa23fb7a9abb8c9dbce1
Maya Gutierrez <mayaguti@usc.edu>
2022-10-23 06:25:33 +0000

    All done

---
+++ heap.h
@@ -0,0 +1,168 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include <functional>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+
+template <typename T, typename PComparator = std::less<T> >
+class Heap
+{
+public:
+  /**
+   * @brief Construct a new Heap object
+   * 
+   * @param m ary-ness of heap tree (default to 2)
+   * @param c binary predicate function/functor that takes two items
+   *          as an argument and returns a bool if the first argument has
+   *          priority over the second.
+   */
+  Heap(int m=2, PComparator c = PComparator());
+
+  /**
+  * @brief Destroy the Heap object
+  * 
+  */
+  ~Heap();
+
+  /**
+   * @brief Push an item to the heap
+   * 
+   * @param item item to heap
+   */
+  void push(const T& item);
+
+  /**
+   * @brief Returns the top (priority) item
+   * 
+   * @return T const& top priority item
+   * @throw std::underflow_error if the heap is empty
+   */
+  T const & top() const;
+
+  /**
+   * @brief Remove the top priority item
+   * 
+   * @throw std::underflow_error if the heap is empty
+   */
+  void pop();
+
+  /// returns true if the heap is empty
+
+  /**
+   * @brief Returns true if the heap is empty
+   * 
+   */
+  bool empty() const;
+
+    /**
+   * @brief Returns size of the heap
+   * 
+   */
+  size_t size() const;
+
+private:
+  /// Add whatever helper functions and data members you need below
+  std::vector<T> data;
+  int m_;
+  PComparator c_;
+  void trickle_down(int i);
+  void trickle_up(int i);
+};
+
+// Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c) : m_(m), c_(c){
+
+}
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap(){
+  
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::trickle_down(int i){
+	int lChild = m_*i +1;
+	if(lChild == i) return;
+	if(lChild > size()) return;
+
+	int swapChild = lChild;
+	int rChild = lChild + m_;
+	for(int j = lChild; ((j <= rChild) && (j <= size())); j++){
+		if(this->c_(data[j], data[swapChild])){
+			swapChild = j;
+		}
+	}
+	if(this->c_(data[swapChild], data[i])){
+		std::swap(data[i], data[swapChild]);
+		trickle_down(swapChild);
+	}
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::trickle_up(int i){
+  if(i==0) return; // invalid index
+  int parent = (i-1)/m_; // find parent index
+  if((parent >= 0)&&(this->c_(data[i], data[parent]))){
+    std::swap(data[i], data[parent]);
+    trickle_up(parent);
+  }
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item){
+  data.push_back(item);
+  trickle_up(data.size()-1);
+}
+
+template <typename T, typename PComparator>
+bool Heap<T, PComparator>::empty() const{
+  return data.size()==0;
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T, PComparator>::size() const{
+  return data.size();
+}
+
+
+// We will start top() for you to handle the case of 
+// calling top on an empty heap
+template <typename T, typename PComparator>
+T const & Heap<T,PComparator>::top() const
+{
+  // Here we use exceptions to handle the case of trying
+  // to access the top element of an empty heap
+  if(empty()){
+    // ================================
+    // throw the appropriate exception
+    // ================================
+    throw std::out_of_range("heap is empty");
+  }
+  // If we get here we know the heap has at least 1 item
+  // Add code to return the top element
+  return data[0];
+}
+
+
+// We will start pop() for you to handle the case of 
+// calling top on an empty heap
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::pop()
+{
+  if(empty()){
+    // ================================
+    // throw the appropriate exception
+    // ================================
+    throw std::out_of_range("heap is empty");
+  }
+	data[0] = data.back();
+	data.pop_back();
+	trickle_down(0);
+}
+
+
+
+#endif
+
